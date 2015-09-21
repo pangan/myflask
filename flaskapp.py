2a170bbba07cb36dc716629518a2731ab6eff6bd
Amir <amir.mofakhar@birdstep.com>
2015-09-21 15:08:53 +0200

    fix 3

--- flaskapp.py
+++ flaskapp.py
@@ -58,8 +58,9 @@ def test():
 		with open(CONFIG_FOLDER+'logins.json') as json_logins:
 			logins = json.load(json_logins)
 			json_logins.close()
-	except Exception:
+	except Exception, e:
 		return "error "
+    
     return "<strong>It's Alive! path = </strong>"
 
 @app.route('/logout')
