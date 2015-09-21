76abcf55d7d5eedb94c171b53b1970c1a569280a
Amir <amir.mofakhar@birdstep.com>
2015-09-21 15:25:15 +0200

    test 10

--- flaskapp.py
+++ flaskapp.py
@@ -54,7 +54,7 @@ def serveStaticResource(resource):
 
 @app.route("/test")
 def test():
-	app_path=os.path.dirname(__file__)
+	app_path=os.path.dirname(os.path.realpath(__file__))
 	try:
 		with open('logins.json') as json_logins:
 			logins = json.load(json_logins)
