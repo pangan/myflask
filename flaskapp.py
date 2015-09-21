7811e0e127a34439599cc76c80caab41302b06f2
Amir <amir.mofakhar@birdstep.com>
2015-09-21 12:38:52 +0200

    adding login

--- flaskapp.py
+++ flaskapp.py
@@ -3,8 +3,42 @@ from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
 
+from functools import wraps
+
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
+
+
+
+def check_auth(username, password):
+	logins = dict()
+
+	try:
+		with open(CONFIG_FOLDER+'/logins.json') as json_logins:
+			logins = json.load(json_logins)
+			json_logins.close()
+	except Exception, e:
+		return False
+	
+	if username in logins:
+		return logins[username] == password
+	return False	
+	
+
+def authenticate():
+	return ('Bad Login! you are not allowed to use this system!',
+	 401, {'WWW-Authenticate': 'Basic realm="Login required"'})
+
+def requires_auth(f):
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		auth = request.authorization
+		if not auth or not check_auth(auth.username, auth.password):
+			return authenticate()
+		return f(*args, **kwargs)
+	return decorated
+
+
 
 @app.route('/')
 def index():
@@ -23,7 +57,10 @@ def test():
 def logout():
 	return render_template('logout.html'), 401
 
-
+@app.route('/admin')
+@requires_auth
+def logout():
+	return render_template('admin.html')
 
 if __name__ == '__main__':
     app.run()
